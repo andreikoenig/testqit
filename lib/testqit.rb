b32a9ad3f7e629fad8a40c6d5bd98a6d4ac0d421
Andrei Koenig <nv1982@gmail.com>
2018-06-17 14:45:17 -0400

    add assert, and asser_equals

--- lib/testqit.rb
+++ lib/testqit.rb
@@ -2,4 +2,26 @@ require "testqit/version"
 
 module Testqit
   # Your code goes here...
+  def assert_equals(a,b)
+    assert(a == b)
+  end 
+
+  def self.assert(test)
+    raise RuntimeError, "Failed test", caller unless test
+  end
+  # Your code goes here...
+
+  # def self.say_hello_world
+  #   puts create_phrase
+  # end
+
+  # def self.create_phrase
+  #   "Hello World from testqit"
+  # end
+
+  # def self.private_two
+  #   'worked'
+  # end
+
+  private_class_method(:assert)
 end
